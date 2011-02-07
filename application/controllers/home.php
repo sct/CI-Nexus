9654fddc981231851bde9f11993e6cae0c5cce03
xploited <jordan@jordanblanco.com>
2011-02-07 14:59:10 +0800

    Added 10 post array to home controller

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -8,7 +8,10 @@ class Home extends CI_Controller {
 
     function index() {
         $this->load->model('post_model');
-        $data['jquery_slider'] = 1;
+        $data_header['jquery_slider'] = 1;
+		
+		$data['posts'] = $this->post_model->getPosts(10);
+		
         $data['categories'] = $this->post_model->getCategories();
         $this->load->view('layout/header',$data);
         $this->load->view('home/index',$data);
